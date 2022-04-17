577dffc49df43472e8503d948c197faf53682b46
0xEstarriol <Estarriol.Vetch@protonmail.com>
2022-04-17 18:22:31 -0400

    add upgradeable

---
+++ contracts/extension/ERC721PsiAddressDataUpgradeable.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+/**
+  ______ _____   _____ ______ ___  __ _  _  _ 
+ |  ____|  __ \ / ____|____  |__ \/_ | || || |
+ | |__  | |__) | |        / /   ) || | \| |/ |
+ |  __| |  _  /| |       / /   / / | |\_   _/ 
+ | |____| | \ \| |____  / /   / /_ | |  | |   
+ |______|_|  \_\\_____|/_/   |____||_|  |_|   
+                                              
+                                            
+ */
+pragma solidity ^0.8.0;
+
+import "../BitMaps.sol";
+import "../ERC721PsiUpgradeable.sol";
+
+
+/**
+    @dev This extension follows the AddressData format of ERC721A, so
+    it can be a dropped-in replacement for the contract that requires AddressData
+*/ 
+abstract contract ERC721PsiAddressDataUpgradeable is ERC721PsiUpgradeable {
+    // Mapping owner address to address data
+    mapping(address => AddressData) _addressData;
+
+    // Compiler will pack this into a single 256bit word.
+    struct AddressData {
+        // Realistically, 2**64-1 is more than enough.
+        uint64 balance;
+        // Keeps track of mint count with minimal overhead for tokenomics.
+        uint64 numberMinted;
+        // Keeps track of burn count with minimal overhead for tokenomics.
+        uint64 numberBurned;
+        // For miscellaneous variable(s) pertaining to the address
+        // (e.g. number of whitelist mint slots used).
+        // If there are multiple variables, please pack them into a uint64.
+        uint64 aux;
+    }
+
+
+    /**
+     * @dev See {IERC721-balanceOf}.
+     */
+    function balanceOf(address owner) 
+        public 
+        view 
+        virtual 
+        override 
+        returns (uint) 
+    {
+        require(owner != address(0), "ERC721Psi: balance query for the zero address");
+        return uint256(_addressData[owner].balance);   
+    }
+
+    /**
+     * @dev Hook that is called after a set of serially-ordered token ids have been transferred. This includes
+     * minting.
+     *
+     * startTokenId - the first token id to be transferred
+     * quantity - the amount to be transferred
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero.
+     * - `from` and `to` are never both zero.
+     */
+    function _afterTokenTransfers(
+        address from,
+        address to,
+        uint256 startTokenId,
+        uint256 quantity
+    ) internal override virtual {
+        require(quantity < 2 ** 64);
+        uint64 _quantity = uint64(quantity);
+
+        if(from != address(0)){
+            _addressData[from].balance -= _quantity;
+        } else {
+            // Mint
+            _addressData[to].numberMinted += _quantity;
+        }
+
+        if(to != address(0)){
+            _addressData[to].balance += _quantity;
+        } else {
+            // Burn
+            _addressData[to].numberBurned -= _quantity;
+        }
+        super._afterTokenTransfers(from, to, startTokenId, quantity);
+    }
+}
